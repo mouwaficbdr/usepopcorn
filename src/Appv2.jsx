c7ba3881c1ab4ac34ab7eda015e55829486ba586
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-04-15 17:25:26 +0100

    Refactor MovieDetails component: améliore la gestion des états et optimise l'effet de mise à jour du titre du document

--- src/Appv2.jsx
+++ src/Appv2.jsx
@@ -202,75 +202,73 @@ function Movie({ movie, onSelectMovie}) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [userRating, setUserRating] = useState('');
 
-  const [movie, setMovie] = useState({})
-  const [isLoading, setIsLoading] = useState(false)
-  const [userRating, setUserRating] = useState("")
-
-  const { Title: title,
+  const {
+    Title: title,
     Year: year,
     Poster: poster,
     Runtime: runtime,
     imdbRating,
-    Plot: plot, 
+    Plot: plot,
     Released: released,
     Actors: actors,
     Director: director,
     Genre: genre,
-  } = movie
+  } = movie;
 
-
+  
   function handleAdd() {
-
     const newWatchedMovie = {
       imdbId: selectedId,
-      title, 
-      year, 
+      title,
+      year,
       poster,
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(' ').at(0)),
-      userRating
-    }
+      userRating,
+    };
 
-    onAddWatched(newWatchedMovie)
-    onCloseMovie()
+    onAddWatched(newWatchedMovie);
+    onCloseMovie();
   }
 
   useEffect(() => {
     const getMovieDetails = async () => {
-      setIsLoading(true)
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
 
-      const data = await res.json()
-      setMovie(data)
-      setIsLoading(false)
-    } 
-    getMovieDetails()
-  }, [selectedId])
+      const data = await res.json();
+      setMovie(data);
+      setIsLoading(false);
+    };
+    getMovieDetails();
+  }, [selectedId]);
 
   useEffect(() => {
     if (!title) return;
-    document.title = `Movie | ${title}`
+    document.title = `Movie | ${title}`;
 
     return () => {
-      document.title = "usePopcorn" 
-    }
-
-  }, [title])
+      document.title = 'usePopcorn';
+    };
+  }, [title]);
 
   useEffect(() => {
-      const callback = (e) => {
-        if (e.code === 'Escape') {
-          onCloseMovie();
-        }
-      };
-      document.addEventListener('keydown', callback);
-      return () => {
-        document.removeEventListener("keydown", callback);
-      };
-    }, [onCloseMovie]);
+    const callback = (e) => {
+      if (e.code === 'Escape') {
+        onCloseMovie();
+      }
+    };
+    document.addEventListener('keydown', callback);
+    return () => {
+      document.removeEventListener('keydown', callback);
+    };
+  }, [onCloseMovie]);
 
   return (
     <div className="details">
@@ -296,8 +294,16 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
           <section>
             <div className="rating">
-                <StarRating maxRating={10} size={24} onSetRating={setUserRating}/>
-                {userRating > 0 && (<button className="btn-add" onClick={handleAdd}>+Add to list</button>)}
+              <StarRating
+                maxRating={10}
+                size={24}
+                onSetRating={setUserRating}
+              />
+              {userRating > 0 && (
+                <button className="btn-add" onClick={handleAdd}>
+                  +Add to list
+                </button>
+              )}
             </div>
             <p>
               <em>{plot}</em>
