69c910abb83ed9d6128e3a7306ff356362624f1b
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-04-16 05:04:07 +0100

    Ajoute le hook personnalisé useMovies pour récupérer les films à partir de l'API OMDB

---
+++ src/useMovies.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react"
+
+const KEY = '8324d8f6';
+
+export function useMovies(query) {
+
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+
+    // callback?.()
+
+    const controller = new AbortController();
+
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      setError('');
+      try {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+
+        if (!res.ok)
+          throw new Error('Something went wrong with fetching movies');
+
+        const data = await res.json();
+
+        if (data.Response === 'False') throw new Error('Movie not found');
+
+        setMovies(data.Search);
+        setError('');
+      } catch (error) {
+        console.error('Error: ', error.message);
+
+        if (error.name !== 'AbortError') {
+          setError(error.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError('');
+      return;
+    }
+
+    fetchMovies();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
+  return {movies, isLoading, error}
+
+}
